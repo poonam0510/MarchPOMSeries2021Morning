691f22ef4e947ec913c0c9c1c0980275fb4bc82f
Poonam <punam0510@gmail.com>
2021-07-07 20:16:33 -0700

    updated  CartPageas per the comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,7 +1,10 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 	
+	private By cartButton = By.id("cart");
 	public CartPage()
 	{
 		System.out.println("Cart page constructor");
@@ -9,6 +12,6 @@ public class CartPage {
 
 	public void addToCart()
 	{
-		System.out.println("add to cart .....");
+		System.out.println("add to cart ....."+cartButton);
 	}
 }
