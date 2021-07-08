12f12c4451db8d7c2af7bcde93cbb21d7114c28a
Poonam <punam0510@gmail.com>
2021-07-07 18:08:51 -0700

    added CartPage

---
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+public class CartPage {
+	
+	public CartPage()
+	{
+		System.out.println("Cart page constructor");
+	}
+
+	public void addToCart()
+	{
+		System.out.println("add to cart .....");
+	}
+}
